356799958e05a81989f4a03dd7fc8457c476cf6a
isaac-smit <isaac-smith@live.nmit.ac.nz>
2024-03-07 13:49:57 +1300

    declaring different variables with corresponding values

---
+++ numbers.js
@@ -0,0 +1,18 @@
+//NOTE -
+/*
+prints
+"10
+20
+30"
+
+const = constant: meaning your declaring a variable as a constant with a value of 10
+let = let: meaning your declaring a variable that can be changed with a value of 20
+var = variable: which an old way of doing both const and let with a value of 30
+*/ 
+const num1 = 10
+console.log(num1)
+let num2 = 20
+console.log(num2)
+var num3 = 30
+console.log(num3)
+//
